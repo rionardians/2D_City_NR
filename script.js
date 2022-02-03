ebc011fc806b62f079ee8d0a9c7334706820548d
Rio Ardian <rioriogue@gmail.com>
2022-02-03 10:04:09 +0700

    12

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "RealityChain",
     productName: "2dverse: Paras World",
-    productVersion: "0.9.1.U",
+    productVersion: "0.9-0203.0940",
 };
 container.setAttribute("isportrait", false);
 
