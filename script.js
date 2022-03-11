d795cc4b7166e191f7433837db384d27d50be637
Rio Ardian <rioriogue@gmail.com>
2022-03-11 11:15:18 +0700

    0

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var config = {
     codeUrl: "Build/Near.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "RealityChain",
-    productName: "2dverse: Paras World",
-    productVersion: "0.9.6.TEST",
+    productName: "2dverse: Near World",
+    productVersion: "0.9.6.N",
 };
 container.setAttribute("isportrait", false);
 
